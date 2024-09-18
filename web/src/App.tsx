476b8ec247b9d9af1092d26afc4428f141ac27d5
HuyTinh <tinhnth15112003@gmail.com>
2024-09-19 00:25:51 +0700

    initial web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
-import "./assets/styles";
 import { ToastContainer } from "react-toastify";
 import { RouterProvider } from "react-router-dom";
 import { RouterHooks } from "./router";
 import { Fragment } from "react/jsx-runtime";
+
+import "./assets/styles/app.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const hookRouter = RouterHooks();
