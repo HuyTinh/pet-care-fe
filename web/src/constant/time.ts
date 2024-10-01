6a03a7c094a03e425213d60c6264b92de694738f
HuyTinh <tinhnth15112003@gmail.com>
2024-10-01 16:22:58 +0700

    update date time

--- web/src/constant/time.ts
+++ web/src/constant/time.ts
@@ -1,42 +1,55 @@
 export const time = [
   {
-    value: "08:00:00",
+    time: "08:00:00",
+    gmt_time: "00:00:00",
     label: "8h",
   },
   {
-    value: "09:00:00",
+    time: "09:00:00",
+    gmt_time: "01:00:00",
     label: "9h",
   },
   {
-    value: "10:00:00",
+    time: "10:00:00",
+    gmt_time: "02:00:00",
     label: "10h",
   },
   {
-    value: "11:00:00",
+    time: "11:00:00",
+    gmt_time: "03:00:00",
     label: "11h",
   },
   {
-    value: "12:00:00",
+    time: "12:00:00",
+    gmt_time: "04:00:00",
     label: "12h",
   },
   {
-    value: "13:00:00",
+    time: "13:00:00",
+    gmt_time: "05:00:00",
     label: "13h",
   },
   {
-    value: "14:00:00",
+    time: "14:00:00",
+    gmt_time: "06:00:00",
     label: "14h",
   },
+  ,
   {
-    value: "15:00:00",
+    time: "15:00:00",
+    gmt_time: "07:00:00",
     label: "15h",
   },
+  ,
   {
-    value: "16:00:00",
+    time: "16:00:00",
+    gmt_time: "08:00:00",
     label: "16h",
   },
+  ,
   {
-    value: "17:00:00",
+    time: "17:00:00",
+    gmt_time: "09:00:00",
     label: "17h",
   },
 ];
