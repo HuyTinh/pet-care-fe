9baea949a0a3705c51202b721016d07658e633e9
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-10-31 23:38:58 +0700

    config done strapi.

--- strapi/src/api/blog-pet-care/controllers/blog-pet-care.ts
+++
@@ -1,7 +0,0 @@
-/**
- * blog-pet-care controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::blog-pet-care.blog-pet-care');
--- strapi/src/api/blog-pet-care/routes/blog-pet-care.ts
+++
@@ -1,7 +0,0 @@
-/**
- * blog-pet-care router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::blog-pet-care.blog-pet-care');
