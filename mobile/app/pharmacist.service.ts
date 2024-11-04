70f782a16e42da03f76343ea1da1a8e01be54585
phucbao20 <lnpbao15@gmail.com>
2024-11-04 15:56:33 +0700

    feature_UI_buill_Mobile

--- mobile/app/pharmacist.service.ts
+++ mobile/app/pharmacist.service.ts
@@ -8,16 +8,15 @@ import * as SecureStore from 'expo-secure-store';
 import { APIReponse } from '../types/api-response';
 export const pharmacistApi = createApi({
     reducerPath: 'pharmacistApi',
-
     tagTypes: ['Prescriptions'],
     // baseQuery: fetchBaseQuery({ baseUrl: 'https://tsm885rc-8888.asse.devtunnels.ms/api/v1' }),
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.mockaron.com/mock/ze9ga5f7xf/' }),
-
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.mockaron.com/mock/ze9ga5f7xf' }),
+    
     endpoints: build => ({
         getPrescription: build.query<any, void>({
             // query: () => '/medical-prescription-service/prescription',
             query: () => `/medical-prescription-service/getAll`,
-            providesTags(result) {
+            providesTags(result : any) {
                 if (result) {
                     const final = [...((result as any)?.data as IPrescription[]).map(({ id }) => ({ type: 'Prescriptions' as const, id })), { type: 'Prescriptions' as const, appointmentId: 'LIST' }]
                     return final;
@@ -61,7 +60,7 @@ export const pharmacistApi = createApi({
             query: () => "/medical_prescription_service/profile"
         }),
         getAccoutById: build.query<APIReponse<Account>, string>({
-            query: (id) => `/medical_prescription_service/profile/${id}`
+             query: (id) => `/medical_prescription_service/profile/${id}`
         })
     })
 })
@@ -71,6 +70,5 @@ export const {
     useGetPrescriptionByIdQuery,
     useGetAccountMutation,
     useGetAllAccountQuery,
-    useGetPrescriptionByAppointmentIdQuery,
-    useGetAccoutByIdQuery
+    useGetPrescriptionByAppointmentIdQuery
 } = pharmacistApi
