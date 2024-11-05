ac2a5329ebdc26f58ebe93005c9dfe09499f83b9
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-11-06 05:27:43 +0700

    .

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,15 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel'],
-      },
-    },
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      'react-native-reanimated/plugin'
-    ],
+    ]
   };
 };
